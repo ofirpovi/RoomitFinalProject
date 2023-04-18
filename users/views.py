4dee5b5de57d01a09ffedcdc6983d200b9255bbc
ofirpovi <ofirpovi@post.bgu.ac.il>
2023-04-18 13:51:19 +0300

    Create urls.py in users app. Create fill_info.html and infoView for first regestration.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
@@ -14,11 +16,11 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(
-                request, 'Account created for {}! You can now login'.format(username))
+                request, 'Hi {}, welcome to ROOMIT! You can now edit your profile'.format(username))
             new_user = authenticate(username=form.cleaned_data['username'],
                                     password=form.cleaned_data['password1'],)
             login(request, new_user)
-            return redirect('profile', new_user.username)
+            return redirect('fill_info', new_user)
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
@@ -26,7 +28,9 @@ def register(request):
 
 @login_required
 def profile(request, username):
+    print('start')
     if request.method == 'POST':
+        print('post')
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
                                    request.FILES,
@@ -34,10 +38,11 @@ def profile(request, username):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated!')
-            return redirect('profile', request.POST['username'])
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile', request.user)
 
     else:
+        print('else')
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
@@ -45,5 +50,23 @@ def profile(request, username):
         'u_form': u_form,
         'p_form': p_form
     }
-
     return render(request, 'users/profile.html', context)
+
+
+def info(request, username):
+    if request.method == 'POST':
+            p_form = ProfileUpdateForm(request.POST,
+                                    request.FILES,
+                                    instance=request.user.profile)
+            if p_form.is_valid():
+                p_form.save()
+                messages.success(request, "Your personal details have been saved and your profile has been created. You can see your profile and edit it at any time by clicking on the 'profile' tab on the top right of the screen.")
+                return redirect('profile', username=request.user.username, permanent=False)
+
+    else:
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'p_form': p_form
+    }
+    return render(request, 'users/fill_info.html', context)
