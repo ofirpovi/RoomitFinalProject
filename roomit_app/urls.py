97ed673daa355f293cf99c2c021176c63fcd4660
eylonsade1 <63980535+eylonsade1@users.noreply.github.com>
2023-04-22 02:51:01 +0300

    deleted unused methods

--- roomit_app/urls.py
+++ roomit_app/urls.py
@@ -1,22 +1,13 @@
-from django.urls import path
 from . import views as app_views
 from django.contrib.auth import views as auth_views
-from django.urls import path, re_path
-from . import views
-# .views import HomePageView, SignupView, FormProfileView, signin, UserHomepageView
+from django.urls import path
+
 
 
 urlpatterns = [
-    # path('home/', app_views.home, name='home'),
-    path('signup/', app_views.signup, name='signup'),
-    path('signin/', auth_views.LoginView.as_view(template_name='signin.html'), name='signin'),
-    path('signout/', auth_views.LogoutView.as_view(template_name='signup.html'), name='signout'),
-    path('info_form/', app_views.profile_info, name='info_form'),
-    path('user_homepage/', app_views.UserHomepageView.as_view(), name='user_homepage'),
     path('', app_views.post_list, name='post_list_page'),
     path('requirementsR/<str:username>/', app_views.requirementsR, name='requirementsR'),
     path('requirementsP/<str:username>/', app_views.requirementsP, name='requirementsP'),
-    # path('post_list/', app_views.post_list, name='post_list_page'),
     path('more/', app_views.more, name='more'),
 ]
 
--- roomit_app/views.py
+++ roomit_app/views.py
@@ -1,15 +1,8 @@
-from django.shortcuts import render, redirect
-from django.template.loader import render_to_string
-from django.views.generic import TemplateView
+from django.shortcuts import redirect
 from rest_framework import status
 from rest_framework.views import APIView
-# from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.authentication import TokenAuthentication
-from django.contrib.auth.models import auth
-from django.views import View
 from django.contrib import messages
-# from formtools.wizard.views import SessionWizardView
 from django.urls import reverse
 from .forms import UpdateRequirementsRForm, UpdateRequirementsPForm
 from django.contrib.auth.models import User
@@ -17,95 +10,18 @@ from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from infscroll.utils import get_pagination
-from django.core.paginator import Paginator
-from django.http import JsonResponse
 from django.shortcuts import render
-from django.template.loader import render_to_string
 from infscroll.views import more_items
 from . import serializers
 from . import models
-from . import permissions
 from .models import RequirementsP, RequirementsR, Scores
 from users.models import Profile, PropertyForOffer
 from .requirements import ListReq, RangReq
 
-#from .forms import InfoForm
 
 
 def home(request):
     return render(request, 'roomit_app/post_list.html')
-
-
-@api_view(['POST', 'GET'])
-def signup(request):
-    if request.method == 'POST':
-        try:
-            serializer = serializers.UserSerializer(data=request.POST)
-            serializer.is_valid(raise_exception=True)
-
-        except (serializers.ValidationError) as e:
-            return Response(data={'request': request, 'error_message': e}, status=status.HTTP_400_BAD_REQUEST)
-
-        user = serializer.save()
-        data = {'user': user}
-        return Response(data, status=status.HTTP_201_CREATED)
-
-    elif request.method == 'GET':
-        return render(request, 'signup.html')
-
-@api_view(['GET', 'POST'])
-def signin(request):
-    if request.method == 'POST':
-        print(request.POST)
-        username = request.POST["email"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            print("Enter if")
-            login(request, user)
-            context = {'user': user}
-            return Response(context= context, status= status.HTTP_200_OK)
-        else:
-            return Response(status= status.HTTP_406_NOT_ACCEPTABLE)
-           
-    elif request.method == 'GET':
-        return render(request, 'signin.html')
-
-
-def signout(request):
-    logout(request)
-    return Response(status= status.HTTP_200_OK)
-
-
-
-@api_view(['GET', 'POST'])
-#@login_required
-def profile_info(request):
-    try:
-        profile = models.Info.objects.get(User_ID=request.user)
-    except models.Info.DoesNotExist:
-        profile = None
-
-    if request.method == 'GET':
-        serializer = serializers.InfoSerializer(profile)
-        #return Response(serializer.data)
-        return render(request, 'info_form.html') 
-
-    elif request.method == 'POST':
-        serializer =serializers.InfoSerializer(data=request.POST)
-        print('Before')
-        if serializer.is_valid():
-            print('After')
-            #print(serializer)
-            serializer.save(user=request.user)
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-def profile(request):
-    return render(request, 'profile.html')
-
 
 @login_required
 def requirementsP(request, username):
