d84015fd1ba63fc402c07abd4006f24a1c1ffd8c
eylonsade1 <63980535+eylonsade1@users.noreply.github.com>
2023-04-22 00:11:09 +0300

    changed admin

--- roomit_app/admin.py
+++ roomit_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Scores, RequirementsR, RequirementsP, Offers
+admin.site.register(Scores)
+admin.site.register(RequirementsR)
+admin.site.register(RequirementsP)
+admin.site.register(Offers)
--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from .models import Profile , PropertyForOffer, Image
-from roomit_app.models import Scores, RequirementsR, RequirementsP, Offers
+
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(PropertyForOffer)
 admin.site.register(Image)
-admin.site.register(Scores)
-admin.site.register(RequirementsR)
-admin.site.register(RequirementsP)
-admin.site.register(Offers)
+
 
