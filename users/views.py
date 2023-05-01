23cd0428e7660b9d070895d612528d0985fc4880
ofirpovi <ofirpovi@post.bgu.ac.il>
2023-05-01 19:35:50 +0300

    Possible to see profiles of other users

--- users/views.py
+++ users/views.py
@@ -39,8 +39,9 @@ def register(request):
 def profile(request, username):
     user = User.objects.get(username=username)
     u_form = UserUpdateForm(instance=user)
-    p_form = ProfileUpdateForm(instance=user)
-    read_only = False
+    p_form = ProfileUpdateForm(instance=user.profile)
+    read_only = False  
+
     if request.user.username != username:
         read_only = True
 
@@ -67,31 +68,6 @@ def profile(request, username):
     print(f'username:{user.username}\nemail: {user.email}')
     return render(request, 'users/other_profile.html', context)
 
-
-# @login_required
-# def profile(request, username):
-#     if request.method == 'POST':
-#         u_form = UserUpdateForm(request.POST, instance=request.user)
-#         p_form = ProfileUpdateForm(request.POST,
-#                                    request.FILES,
-#                                    instance=request.user.profile)
-#         if u_form.is_valid() and p_form.is_valid():
-#             u_form.save()
-#             p_form.save()
-#             update_scores(request)
-#             messages.success(request, f'Your profile has been updated!')
-#             return redirect('profile', request.user)
-
-#     else:
-#         u_form = UserUpdateForm(instance=request.user)
-#         p_form = ProfileUpdateForm(instance=request.user.profile)
-
-#     context = {
-#         'u_form': u_form,
-#         'p_form': p_form
-#     }
-#     return render(request, 'users/profile.html', context)
-
 @login_required
 def info(request, username):
     if request.method == 'POST':
@@ -102,7 +78,6 @@ def info(request, username):
             p_form.save()
             messages.success(request, "Your personal details have been saved and your profile has been created. You can see your profile and edit it at any time by clicking on the 'profile' tab on the top right of the screen.")
             return render(request, 'users/choose_status.html')
-            # return redirect('profile', username=request.user.username, permanent=False)
 
     else:
         p_form = ProfileUpdateForm(instance=request.user.profile)
@@ -158,10 +133,10 @@ def create_property_offer_view(request):
 
 @login_required
 def set_status(request):
+    user = request.user
     if request.method == 'GET':
-        Profile.objects.filter(user=request.user).update(
-            profile_status=request.GET['status'])
+        Profile.objects.filter(user = user).update(profile_status= request.GET['status'])
         if request.GET['status'] == 'insert in':
             return redirect('property-offer-create')
         else:
-            return redirect('requirementsP', request.user)
+            return redirect('requirementsP', user)
