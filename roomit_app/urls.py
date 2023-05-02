33f6e0cf2fb6752d8012dfddc633566c301d9cbc
eylonsade1 <63980535+eylonsade1@users.noreply.github.com>
2023-05-02 15:30:48 +0300

    merged django branch

--- roomit_app/urls.py
+++ roomit_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', app_views.post_list, name='post_list_page'),
     path('requirementsR/<str:username>/', app_views.requirementsR, name='requirementsR'),
     path('requirementsP/<str:username>/', app_views.requirementsP, name='requirementsP'),
-    path('more/', app_views.more, name='more'),
-    path('search-results', app_views.search, name='search-results')
+    path('more/', app_views.more, name='more')
+    # path('search-results', app_views.search, name='search-results')
 ]
 
