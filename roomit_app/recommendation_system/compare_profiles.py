dd91fc41d82ecc35c5d517839b9a2788ec372aa4
eylonsade1 <63980535+eylonsade1@users.noreply.github.com>
2023-05-20 22:50:26 +0300

    rec sys start

---
+++ roomit_app/recommendation_system/compare_profiles.py
@@ -0,0 +1,43 @@
+from datetime import date
+from users.models import Profile
+
+# todo: add consideration for idk answers and no answer
+def compare_profiles(profile1: Profile, profile2: Profile):
+    counter, total_fields = 0, 9
+    if profile1.profile_status == profile2.profile_status:
+        if calculate_age(profile1.birthdate) == calculate_age(profile2.birthdate):
+            counter += 1
+        if profile1.gender == profile2.gender:
+            counter += 1
+        if profile1.occupation == profile2.occupation:
+            counter += 1
+        if profile1.smoker == profile2.smoker:
+            counter += 1
+        if profile1.diet == profile2.diet:
+            counter += 1
+        if profile1.status == profile2.status:
+            counter += 1
+        if profile1.hospitality == profile2.hospitality:
+            counter += 1
+        if profile1.kosher == profile2.kosher:
+            counter += 1
+        if profile1.expense_management == profile2.expense_management:
+            counter += 1
+    return counter / total_fields
+
+
+def calculate_age(dob):
+    today = date.today()
+    # check that birthday is of type date
+    if type(dob) is not date:
+        raise TypeError("Birthday must be a date")
+    # check that the birthdate has past
+    if dob > today:
+        raise ValueError("Birthday must be a date from the past")
+    # calculate age from given birthdate
+    age = today.year - dob.year - ((today.month, today.day) <
+         (dob.month, dob.day))
+    return age
+
+
+
