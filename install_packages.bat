66cc7c0043907fb53b06deb773cd34f498020031
eylonsade1 <63980535+eylonsade1@users.noreply.github.com>
2023-05-21 20:58:06 +0300

    added bat file to easily install all python packages - need to test it. still need to add instructions for using it to the readme file

---
+++ install_packages.bat
@@ -0,0 +1,34 @@
+@echo off
+echo Installing required Python packages for ROOMIT project...
+
+REM Install os
+pip install os
+
+REM Install sys
+pip install sys
+
+REM Install django
+pip install django
+
+REM Install django_filters
+pip install django_filters
+
+REM Install rest_framework
+pip install djangorestframework
+
+REM Install datetime
+pip install datetime
+
+REM Install abc
+pip install abc
+
+REM Install math
+pip install math
+
+REM Install unittest
+pip install unittest
+
+REM Install infscroll
+pip install infscroll
+
+echo Installation complete.
