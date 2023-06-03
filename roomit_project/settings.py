ad0656ba68274a042e693e0a8fdeef6c876fe723
ofirpovi <62700384+ofirpovi@users.noreply.github.com>
2023-06-03 16:14:41 +0300

    Update settings.py

--- roomit_project/settings.py
+++ roomit_project/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'crispy_forms',
     'crispy_bootstrap5',
-    'verify_email.apps.VerifyEmailConfig',
+    #'verify_email.apps.VerifyEmailConfig',
     'phonenumber_field',
     'infscroll',
     'djmoney',
