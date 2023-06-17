b91a366e939da8b7c1f857c9cea68d361e4311b4
eylonsade1 <63980535+eylonsade1@users.noreply.github.com>
2023-06-17 20:33:06 +0300

    render - whitenoise and collect static update

--- roomit_project/settings.py
+++ roomit_project/settings.py
@@ -54,10 +54,12 @@ INSTALLED_APPS = [
     'django.test',
     # 'djongo',
     'django_extensions',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -170,7 +172,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
